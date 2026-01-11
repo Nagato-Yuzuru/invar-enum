fae33a3bc940ba44a733a11a927dd9ac22adcd90
Nagato Yuzuru <x4.nagato.yuzuru@gmail.com>
2026-01-11 23:07:48 +0800

    feat(renderer): add renderer types and structure for file generation
    Signed-off-by: Nagato Yuzuru <x4.nagato.yuzuru@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Nagato-Yuzuru/invar-enum
 
 go 1.25
+
+require github.com/iancoleman/strcase v0.3.0
--- internal/renderer/renderer.go
+++ internal/renderer/renderer.go
@@ -1,1 +1,62 @@
 package renderer
+
+import (
+	"context"
+
+	"github.com/Nagato-Yuzuru/invar-enum/pkg/strutil"
+)
+
+type GeneratedFile struct {
+	FileName string
+	Content  []byte
+}
+type FileModel struct {
+	Path string // e.g. "pkg/payment/payment_invar.go"
+	// PackageName Java / go need. e.g. "payment" (Go package) or namespace
+	PackageName string
+	Imports     []string // e.g. ["database/sql", "fmt"]
+
+	Enums []EnumModel
+
+	Options RenderOptions
+}
+
+type (
+	EnumModel struct {
+		// ProtoName original proto info
+		ProtoName    string
+		ProtoRawName string
+		Name         strutil.CaseSet
+		Comments     []string
+		DefaultItem  *EnumItem
+		Items        []EnumItem
+		// Options if nil inherit from FileModel
+		Options *RenderOptions
+	}
+
+	EnumItem struct {
+		Key          strutil.CaseSet
+		StrValue     string
+		Numeric      int32 // Protobuf always positive
+		Label        string
+		Comments     []string
+		IsDeprecated bool
+		ProtoKey     string
+	}
+)
+
+type RenderOptions struct {
+	UseNumericValue  bool
+	UseStrValue      bool
+	GenerateLabel    bool
+	GenerateProtoMap bool
+	FileHeader       string
+	Extras           ExtraOptions
+}
+
+// ExtraOptions Unuse in MVP
+type ExtraOptions map[string]any
+
+type Renderer interface {
+	Render(ctx context.Context, files []FileModel) ([]GeneratedFile, error)
+}
